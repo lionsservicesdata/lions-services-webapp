003a3a29453d854e4ab7e89eb89089c9abf500ff
Noah-Harris1 <lionsservicesdata@gmail.com>
2023-03-21 20:19:02 -0400

    Bug Fix

--- src/Components/Production System/ProductionSystems.jsx
+++ src/Components/Production System/ProductionSystems.jsx
@@ -5,13 +5,17 @@ import { useState } from 'react';
 import { Production_System } from '../../Util/Production_System';
 
 
+
 export const ProductionSystems = () => {
-	var[data, setData] = useState('')
+	var[data, setData] = useState(new Production_System('','',''))
 
 	const postData = () => {
+		try{
+			setData(new Production_System(document.getElementById('productionSystem').value, document.getElementById('productName').value, String(new Date.getTime())))
+		}catch(e) {
+			console.log(e)
+		}
 		
-		setData(new Production_System(document.getElementById('productionSystem').value,document.getElementById('productName').value, String(new Date.getTime())))
-
 		axiosPost(data,'Production_Systems').then((r)=> {
 			console.log(r)
 		}).catch((e)=>{
@@ -25,10 +29,13 @@ export const ProductionSystems = () => {
 			<Navbar></Navbar>
 			<form onSubmit={postData()}>
 				<label>Name of Production System:</label>
+				<br/>
 					<input type="text" id="productionSystem" name="productionSystem"/>
 				<br/>
 				<label>Product Name:</label>
+				<br/>
 					<input type="text" id="productName" name="productName"/>
+				<br/>
 				<input type="submit" value="Submit"/>
 			</form>
 		</div>
