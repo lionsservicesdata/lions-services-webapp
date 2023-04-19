6dba6e8fb04b855cc36b46acaa55ab48b1a8b0dc
Noah-Harris1 <lionsservicesdata@gmail.com>
2023-04-18 22:23:17 -0400

    Update

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,4 +1,3 @@
-import MaterialTable from '@material-table/core'
 import { Navbar } from '../Common/Navbar/Navbar'
 import React from 'react'
 
@@ -6,7 +5,6 @@ export const Home = () => {
 	return (
 		<div className='homePage'>
 			<Navbar></Navbar>
-			<MaterialTable/>
 		</div>
 	)
 }
